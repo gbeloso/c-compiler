26994d2863f8bd2a80c8365375ac1142f7852159
gbeloso <gabrielbeloso62@gmail.com>
2022-12-13 17:05:16 -0300

    adaptado aos terminais do c-

--- c-compiler.c
+++ c-compiler.c
@@ -4,7 +4,7 @@
 
 #define tamMax 200
 
-enum {FIM = -1, ID = 0, NUM = 1, OP, COMP, ERRO, DELIMITADOR};
+enum {FIM = -1, ID = 0, INT = 1, VOID, IF, ELSE, WHILE, RETURN, NUM, SOMA, MULT, IGUAL, RELACIONAL, LBRACKET, RBRACKET, LPAREN, RPAREN, LBRACE, RBRACE, COMMA, SEMICOLON, ERRO};
 
 int linhas = 1;
 int indexBufferEntrada = 0;
@@ -19,7 +19,7 @@ int tabelaTransicao[9][12];
 typedef struct 
 {
     int type;
-    char lexem[200];
+    char * lexem;
 } token;
 
 int mapeamento(int caracter){
@@ -106,6 +106,7 @@ int getInput(){
 token myGetToken(){
     int estadoAtual = 0;
     int temp;
+    bufferSaida[0] = '\0';
     token aux;
     while ((estadoAtual < 9))
     {
@@ -134,37 +135,79 @@ token myGetToken(){
     {
         if(mapeamento(bufferSaida[indexBufferSaida-1]) == 5){
             bufferSaida[indexBufferSaida] = '\0';
-            printf("<OP, %s>\n", bufferSaida);
-            aux.type = OP;
-            bufferSaida[0] = '\0';
+            fprintf(escrita, "<SOMA, %s>", bufferSaida);
+            aux.type = SOMA;
             indexBufferSaida = 0;
         }
-        else{
+        else if(mapeamento(bufferSaida[indexBufferSaida-2]) == 8){
             bufferSaida[indexBufferSaida - 1] = '\0';
-            printf("<OP, %s>\n", bufferSaida);
-            aux.type = OP;
-            bufferSaida[0] = '\0';
+            fprintf(escrita, "<IGUAL, %s>", bufferSaida);
+            aux.type = IGUAL;
             indexBufferSaida = 0;
             indexBufferEntrada--;
+        }
+        else if(mapeamento(bufferSaida[indexBufferSaida-2]) == 6){
+            bufferSaida[indexBufferSaida - 1] = '\0';
+            fprintf(escrita, "<MULT, %s>", bufferSaida);
+            aux.type = MULT;
+            indexBufferSaida = 0;
+            indexBufferEntrada--;
+        }
+        else{
+            bufferSaida[indexBufferSaida] = '\0';
+            fprintf(escrita, "<MULT, %s>", bufferSaida);
+            aux.type = MULT;
+            indexBufferSaida = 0;
         }
         return aux;
     }
     else if (estadoAtual == 10)
     {
         bufferSaida[indexBufferSaida - 1] = '\0';
-        printf("<ID, %s>\n", bufferSaida);
-        aux.type = ID;
-        bufferSaida[0] = '\0';
         indexBufferSaida = 0;
         indexBufferEntrada--;
+        if (strcmp(bufferSaida, "int") == 0)
+        {
+            fprintf(escrita, "<int, %s>", bufferSaida);
+            aux.type = INT;
+        }
+        else if (strcmp(bufferSaida, "void") == 0)
+        {
+            fprintf(escrita, "<void, %s>", bufferSaida);
+            aux.type = VOID;
+        }
+        else if (strcmp(bufferSaida, "if") == 0)
+        {
+            fprintf(escrita, "<if, %s>", bufferSaida);
+            aux.type = IF;
+        }
+        else if (strcmp(bufferSaida, "else") == 0)
+        {
+            fprintf(escrita, "<else, %s>", bufferSaida);
+            aux.type = ELSE;
+        }
+        else if (strcmp(bufferSaida, "return") == 0)
+        {
+            fprintf(escrita, "<return, %s>", bufferSaida);
+            aux.type = RETURN;
+        }
+        else if (strcmp(bufferSaida, "while") == 0)
+        {
+            fprintf(escrita, "<while, %s>", bufferSaida);
+            aux.type = WHILE;
+        }
+        else 
+        {
+            fprintf(escrita, "<ID, %s>", bufferSaida);
+            aux.type = ID;
+        }
         return aux;
     }
     else if (estadoAtual == 11)
     {
         bufferSaida[indexBufferSaida - 1] = '\0';
-        printf("<Num, %s>\n", bufferSaida);
+        fprintf(escrita, "<Num, %s>", bufferSaida);
         aux.type = NUM;
-        bufferSaida[0] = '\0';
         indexBufferSaida = 0;
         indexBufferEntrada--;
         return aux;
@@ -173,17 +216,15 @@ token myGetToken(){
     {
         if(bufferSaida[indexBufferSaida-1] == 33){
             bufferSaida[indexBufferSaida - 1] = '\0';
-            printf("<Erro, %s>\n", bufferSaida);
+            fprintf(escrita, "<Erro, %s>", bufferSaida);
             aux.type = ERRO;
-            bufferSaida[0] = '\0';
             indexBufferSaida = 0;
             indexBufferEntrada--;
         }
         else{
             bufferSaida[indexBufferSaida] = '\0';
-            printf("<Erro, %s>\n", bufferSaida);
+            fprintf(escrita, "<Erro, %s>", bufferSaida);
             aux.type = ERRO;
-            bufferSaida[0] = '\0';
             indexBufferSaida = 0;
         }
         return aux;
@@ -191,40 +232,71 @@ token myGetToken(){
     else if (estadoAtual == 13)
     {
         bufferSaida[indexBufferSaida] = '\0';
-        printf("<Delimitador, %s>\n", bufferSaida);
-        aux.type = DELIMITADOR;
-        bufferSaida[0] = '\0';
+        if (bufferSaida[indexBufferSaida -1] == 40)
+        {
+            fprintf(escrita, "<LPAREN, %s>", bufferSaida);
+            aux.type = LPAREN;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 41)
+        {
+            fprintf(escrita, "<RPAREN, %s>", bufferSaida);
+            aux.type = RPAREN;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 91)
+        {
+            fprintf(escrita, "<LBRACKET, %s>", bufferSaida);
+            aux.type = LBRACKET;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 93)
+        {
+            fprintf(escrita, "<RBRACKET, %s>", bufferSaida);
+            aux.type = RBRACKET;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 123)
+        {
+            fprintf(escrita, "<LBRACE, %s>", bufferSaida);
+            aux.type = LBRACE;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 125)
+        {
+            fprintf(escrita, "<RBRACE, %s>", bufferSaida);
+            aux.type = RBRACE;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 44)
+        {
+            fprintf(escrita, "<COMMA, %s>", bufferSaida);
+            aux.type = COMMA;
+        }
+        else if (bufferSaida[indexBufferSaida -1] == 59)
+        {
+            fprintf(escrita, "<SEMICOLON, %s>", bufferSaida);
+            aux.type = SEMICOLON;
+        }
         indexBufferSaida = 0;
         return aux;
     }
     else if (estadoAtual == 14)
     {
-        if(bufferSaida[indexBufferSaida-1] == 61){
-            bufferSaida[indexBufferSaida] = '\0';
-            printf("<Comparacao, %s>\n", bufferSaida);
-            aux.type = COMP;
-            bufferSaida[0] = '\0';
-            indexBufferSaida = 0;
-        }
-        else{
-            bufferSaida[indexBufferSaida - 1] = '\0';
-            printf("<Comparacao, %s>\n", bufferSaida);
-            aux.type = COMP;
-            bufferSaida[0] = '\0';
-            indexBufferSaida = 0;
-            indexBufferEntrada--;
-        }
+        bufferSaida[indexBufferSaida] = '\0';
+        fprintf(escrita, "<Relacional, %s>", bufferSaida);
+        aux.type = RELACIONAL;
+        indexBufferSaida = 0;
         return aux;
     }
     else if (estadoAtual == 15)
     {
         bufferSaida[indexBufferSaida] = '\0';
-        printf("<Fim, %s>\n", bufferSaida);
+        fprintf(escrita, "<Fim>");
         aux.type = FIM;
         //aux.lexem = bufferSaida;
-        bufferSaida[0] = '\0';
         indexBufferSaida = 0;
         return aux;
+    }
+    else if(estadoAtual == 16){
+        bufferSaida[indexBufferSaida] = '\0';
+        fprintf(escrita, "%s", bufferSaida);
+        aux.type = SOMA;
+        indexBufferSaida = 0;
     }
     
     
@@ -237,20 +309,20 @@ int main(int argc, char ** argv){
     token next;
     int aux = 0;
     arquivo = fopen("programa.c", "r");
-    //escrita = fopen("tokens.txt", "w");
+    escrita = fopen("tokens.txt", "w");
 
     next.type = 0;
 
     tabelaTransicao[0][0] = 1;
     tabelaTransicao[0][1] = 2;
     tabelaTransicao[0][2] = 6;
-    tabelaTransicao[0][3] = 8;
+    tabelaTransicao[0][3] = 9;
     tabelaTransicao[0][4] = 7;
     tabelaTransicao[0][5] = 9;
     tabelaTransicao[0][6] = 3;
     tabelaTransicao[0][7] = 13;
-    tabelaTransicao[0][8] = 7;
-    tabelaTransicao[0][9] = 0;
+    tabelaTransicao[0][8] = 8;
+    tabelaTransicao[0][9] = 16;
     tabelaTransicao[0][10] = 12;
     tabelaTransicao[0][11] = 15;
 
@@ -304,7 +376,7 @@ int main(int argc, char ** argv){
     tabelaTransicao[4][8] = 4;
     tabelaTransicao[4][9] = 4;
     tabelaTransicao[4][10] = 4;
-    tabelaTransicao[4][11] = 0;
+    tabelaTransicao[4][11] = 12;
     
     tabelaTransicao[5][0] = 4;
     tabelaTransicao[5][1] = 4;
@@ -317,7 +389,7 @@ int main(int argc, char ** argv){
     tabelaTransicao[5][8] = 4;
     tabelaTransicao[5][9] = 4;
     tabelaTransicao[5][10] = 4;
-    tabelaTransicao[5][11] = 0;
+    tabelaTransicao[5][11] = 12;
 
     tabelaTransicao[6][0] = 12;
     tabelaTransicao[6][1] = 12;
@@ -341,9 +413,9 @@ int main(int argc, char ** argv){
     tabelaTransicao[7][6] = 14;
     tabelaTransicao[7][7] = 14;
     tabelaTransicao[7][8] = 14;
-    tabelaTransicao[7][9] = 9;
-    tabelaTransicao[7][10] = 9;
-    tabelaTransicao[7][11] = 9;
+    tabelaTransicao[7][9] = 14;
+    tabelaTransicao[7][10] = 14;
+    tabelaTransicao[7][11] = 14;
     
     tabelaTransicao[8][0] = 9;
     tabelaTransicao[8][1] = 9;
@@ -353,12 +425,12 @@ int main(int argc, char ** argv){
     tabelaTransicao[8][5] = 9;
     tabelaTransicao[8][6] = 9;
     tabelaTransicao[8][7] = 9;
-    tabelaTransicao[8][8] = 9;
+    tabelaTransicao[8][8] = 14;
     tabelaTransicao[8][9] = 9;
     tabelaTransicao[8][10] = 9;
     tabelaTransicao[8][11] = 9;
 
-    if ((arquivo == NULL))
+    if ((arquivo == NULL) || (escrita == NULL))
         printf("deu ruim\n");
 
     while (next.type != FIM)
--- programa.c
+++ programa.c
@@ -1,25 +1,25 @@
 /* Um programa para ordenação por seleção de uma matriz com dez elementos. */
+
 int x[10];
-int minloc ( int a[], int low, int high ) 
-{ 
+
+int minloc ( int a[], int low, int high ) { 
     int i; 
     int x; 
     int k; 
     k = low; 
     x = a[low]; 
     i = low + 1; 
-    while (i < high) 
-    { 
+    while (i < high) { 
         if (a[i] < x) { 
             x = a[i]; 
             k = i; 
         } 
-    i = i + 1;
-    } 
+        i = i + 1;
+    }
     return k; 
 }
-void sort( int a[], int low, int high) 
-{ 
+
+void sort( int a[], int low, int high) {
     int i; 
     int k; 
     i = low;
@@ -30,19 +30,18 @@ void sort( int a[], int low, int high)
         a[k] = a[i]; 
         a[i] = t; 
         i = i + 1;
-    } 
+    }
 }
-void main(void) { 
-    int i; 
-    i = 0;
+void main(void) {
+    int i; i = 0;
     while (i < 10) { 
-        x[i] = input();
+        x[i] = input(); 
         i = i + 1; 
     }
     sort(x,0,10); 
     i = 0;
     while (i < 10) { 
-        output(x[i]);   
+        output(x[i]); 
         i = i + 1;
     }
 }
